52274dace78aca07b5a147b8e93c3c91217cd48c
m4k1m4 <m4k1m4fun@gmail.com>
2024-02-16 12:15:41 +0300

    tg_bot

--- handlers/user_commands.py
+++ handlers/user_commands.py
@@ -1,8 +1,9 @@
 import random
 
-from aiogram import Router, Bot
+from aiogram import Router, Bot, F
 from aiogram.types import Message
 from aiogram.filters import Command, CommandObject, CommandStart
+from aiogram.enums.dice_emoji import DiceEmoji
 
 from keyboards import reply
 
@@ -19,6 +20,15 @@ async def get_random_number(message: Message, command: CommandObject):
 
   await message.reply(f"Random number: {rnum}")
 
+@router.message(F.text == "play")
+async def play_games(message: Message):
+  x = await message.answer_dice(DiceEmoji.DICE)
+  print(x.dice.value)  
+
 @router.message(Command("test"))
 async def test(message: Message, bot: Bot):
   await bot.send_message(message.chat.id, "test") 
+
+@router.message()
+async def echo(message: Message):
+  await message.answer(f"I don't understand you:)")
